a96f3a35068bbc049f1c36b221234c346d8d61c2
JimyFengqi <jmps515@163.com>
2018-10-19 17:59:06 +0800

    modify python method
    修改python的方法

--- spider_for_kugou/python/kuwo.py
+++ spider_for_kugou/python/kuwo.py
@@ -4,6 +4,9 @@ import urllib.parse
 from pyquery import PyQuery as pq
 import webbrowser as wb
 import time
+import json
+import redis
+r = redis.Redis(host='127.0.0.1', port=6379,db=0)
 
 s = requests.session()
 #filename = '周杰伦'
@@ -12,7 +15,7 @@ s = requests.session()
 base_url = 'http://www.kuwo.cn/yinyue/54994175?catalog=yueku2016'
 
 base_number = re.findall(r"(?:http://www.kuwo.cn/yinyue/)(\d+)(?:\?catalog=yueku2016)", base_url)[0]
-def get_url():
+def get_url(number):
     headers = {
         'Accept': '*/*',
         'Accept-Encoding': 'identity;q=1, *;q=0',
@@ -24,10 +27,13 @@ def get_url():
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'Referer':base_url
     }
-    url = "http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_"+ base_number +"&type=convert_url&response=res"
-
-    r = s.get(url, headers=headers, allow_redirects=False)
-    return r.headers['Location']
+    #url = "http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_"+ base_number +"&type=convert_url&response=res"
+    #r = s.get(url, headers=headers, allow_redirects=False)
+    url = "http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_"+ number +"&type=convert_url&response=res"
+    r = s.get(url,allow_redirects=False)
+    accpath= r.headers['Location']
+    print(accpath)
+    return accpath
 
 def get_aac(url):
 	base_host = re.findall(r"(?:http://)(.*)", url)[0]
@@ -43,6 +49,7 @@ def get_aac(url):
             'Range': 'bytes=0-'
            }
 	r = s.get(url, headers=headers, stream=True)
+	print(r)
 	return r
 def save_aac(filename, res):
 	with open(filename, 'wb') as fd:
@@ -50,13 +57,42 @@ def save_aac(filename, res):
 	        fd.write(chunk)
 
 
-def getRanklist():
-	url='www.kuwo.cn/bang/content?name=%s' % urllib.parse.quote('酷我热歌榜')
+def getRanklist(rankType):
+	headers ={
+            'Accept': '*/*',
+            'Accept-Encoding': 'identity;q=1, *;q=0',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Host': 'www.kuwo.cn',
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+            'Range': 'bytes=0-'
+           }
+	#rankType='酷我热歌榜'
+	url='http://www.kuwo.cn/bang/content?name=%s' % urllib.parse.quote(rankType)
+	#print(url)
+	#url='http://www.kuwo.cn/bang/content?name=%E9%85%B7%E6%88%91%E7%83%AD%E6%AD%8C%E6%A6%9C'
+	#print(url)
 	html=s.get(url).content.decode('utf-8')
 	doc=pq(html)
 	singlist=doc('div.name')
 	print(len(singlist))
-
+	Ranklist=[]
+	for index,sing in enumerate(singlist.items()):
+		singurl=sing('a').attr('href')
+		number=singurl.split('?')[0].split('/')[-1]
+		#name=sing('a').text()
+		rank=index+1
+		print(singurl,number,rank)
+		'''
+		data=getjson(number)
+		data['rank']=rank
+		data['singurl']=singurl
+		data['rank']=rank
+		Ranklist.append(data)
+	json.dumps(Ranklist)
+	return Ranklist
+	'''
 
 def getjson(number):
 	base='http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_'
@@ -74,32 +110,55 @@ def getjson(number):
 	aacpath=doc('aacpath').text()
 	aacdl=doc('aacdl').text()
 
-
-
 	#print(path)
-	path='http://'+mp3dl+'/resource/'+path
-	print(path)
+	wmapath='http://'+mp3dl+'/resource/'+path
+	print(wmapath)
 
 	mp3path='http://'+mp3dl+'/resource/'+mp3path
 	print(mp3path)
-	#wb.open_new(mp3path)
-	time.sleep(2)
-
 	aacpath='http://'+aacdl+'/resource/'+aacpath
-	time.sleep(1)
-	
 	print(aacpath)
-	#wb.open_new(aacpath)
-
-
-
 	print(name,singer,artist)
+	data = {"singer":singer,
+	"singer":singer,
+	"name":name,
+	"wmapath":wmapath,
+	"mp3path":mp3path,
+	"aacpath":aacpath,
+	}
+	return data
 
 
-print('base_number [%s]' % base_number)
-url=get_url()
-#wb.open_new(url)
-time.sleep(1)
-print(url)	        
-getjson(base_number)
-getRanklist()
+def getRankNamelist():
+	headers ={
+            'Accept': '*/*',
+            'Accept-Encoding': 'identity;q=1, *;q=0',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Host': 'www.kuwo.cn',
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+            'Range': 'bytes=0-'
+           }
+	url='http://www.kuwo.cn/bang/index'
+	#print(url)
+	#url='http://www.kuwo.cn/bang/content?name=%E9%85%B7%E6%88%91%E7%83%AD%E6%AD%8C%E6%A6%9C'
+	#print(url)
+	html=s.get(url).content.decode('utf-8')
+	doc=pq(html)
+
+	Ranknamelist=doc("div.leftNav")('li')
+
+
+	for index,sing in enumerate(Ranknamelist.items()):
+		#print('{%s："%s"},' % (index,sing.text()) )
+		print(sing.text(),sing.attr('data-bangid') )
+		#print((index,sing.text()))
+		#print('"%s",' % sing.text())
+		#getRanklist(sing.text())
+#save_aac('那一刻是你',get_aac(get_url('54994175')))
+#getjson('54994175')
+#data=getRanklist()
+#r.lpush("HotRank",data)
+
+getRankNamelist()
